cbe381287e9e855feaf03720bd8399e0b287503e
MokuKen <91607111+MokuKen@users.noreply.github.com>
2022-03-14 21:50:39 +0800

    Add files via upload

---
+++ CwallpaperE60.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+wallDir="$HOME/myfiles/wallpaper/zaki/"
+
+while true
+do
+	randomWallpaper=$(ls $wallDir | shuf -n 1)
+	echo $randomWallpaper
+	feh --bg-fill $wallDir$randomWallpaper
+	sleep 60
+done
+
